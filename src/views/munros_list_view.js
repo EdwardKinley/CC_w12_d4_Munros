2036af9b70cc58d2840f4e93361cf3c3d829be12
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-08-09 14:26:11 +0100

    dropdown menu works

--- src/views/munros_list_view.js
+++ src/views/munros_list_view.js
@@ -9,27 +9,44 @@ MunroListView.prototype.bindEvents = function () {
   PubSub.subscribe('Munros:data-ready', (evt) => {
     this.munros = evt.detail;
     this.displayDropdown(this.munros);
-    this.render();
+    this.render(this.munros);
   });
+
+  document.querySelector('#dropdown').addEventListener('change', (evt) => {
+      this.container.innerHTML = '';
+      const selectedRegion = evt.target.value;
+      var munrosOfRegion = this.filterMunros(selectedRegion);
+      this.render(munrosOfRegion);
+  })
+
 };
 
 MunroListView.prototype.displayDropdown = function(munros) {
   var regions = munros.map(munro => munro.region);
+
   var uniqueRegions = Array.from(new Set(regions));
-  var dropdown = this.container.appendChild(document.createElement('select'));
+
+  var dropdown = document.querySelector("#dropdown")
+
+
   uniqueRegions.forEach((uniqueRegion, index) => {
     const option = document.createElement('option');
     option.textContent = uniqueRegion;
-    option.value = index;
+    option.value = uniqueRegion;
     dropdown.appendChild(option);
   });
+
 };
 
-MunroListView.prototype.render = function () {
-  this.munros.forEach((munro) => {
+MunroListView.prototype.render = function (data) {
+  data.forEach((munro) => {
     const munroView = new MunroView(this.container, munro);
     munroView.render();
   });
+};
+
+MunroListView.prototype.filterMunros = function (region) {
+  return this.munros.filter(munro => munro.region === region);
 };
 
 module.exports = MunroListView;
