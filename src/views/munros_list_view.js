a1deccb5307f6e63c5b7bc3faf53fbc2fafb46aa
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-08-09 13:55:10 +0100

    displays regions in dropdown menu

--- src/views/munros_list_view.js
+++ src/views/munros_list_view.js
@@ -8,7 +8,20 @@ const MunroListView = function (container) {
 MunroListView.prototype.bindEvents = function () {
   PubSub.subscribe('Munros:data-ready', (evt) => {
     this.munros = evt.detail;
+    this.displayDropdown(this.munros);
     this.render();
+  });
+};
+
+MunroListView.prototype.displayDropdown = function(munros) {
+  var regions = munros.map(munro => munro.region);
+  var uniqueRegions = Array.from(new Set(regions));
+  var dropdown = this.container.appendChild(document.createElement('select'));
+  uniqueRegions.forEach((uniqueRegion, index) => {
+    const option = document.createElement('option');
+    option.textContent = uniqueRegion;
+    option.value = index;
+    dropdown.appendChild(option);
   });
 };
 
